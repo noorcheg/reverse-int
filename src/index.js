e946b05690a2a382d6e0c0b23321884b3092f889
Ainur Galiakberov <norocheg@gmail.com>
2022-01-04 16:01:41 +0500

    feat: task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  let nStr = String(Math.abs(n));
+  let nLength = nStr.length;
+  let result = '';
+  for (let count = nLength - 1; count >= 0; count--) {
+    result = result + nStr[count];
+  }
+  return Number(result);
 }
